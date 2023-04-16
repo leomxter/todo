1c3c0c36b5df5bf509640d1aeeb15f5523a2550a
Elmyrza Doolotbekov <elmyrza.do@gmail.com>
2023-04-16 19:03:48 +0600

    2nd

--- apps/models.py
+++ apps/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    phone_number = models.CharField(max_length=20, unique=True)
+    age = models.PositiveIntegerField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Todo(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    description = models.TextField(blank=True)
+    is_completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to='todo_images/')
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
