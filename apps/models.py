0b40848d20d306e6a4233e5ee7d92925b2a3b321
Elmyrza Doolotbekov <elmyrza.do@gmail.com>
2023-04-16 19:15:21 +0600

    changed

--- apps/models.py
+++ apps/models.py
@@ -1,15 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+
 class User(AbstractUser):
-    phone_number = models.CharField(max_length=20, unique=True)
-    age = models.PositiveIntegerField(null=True, blank=True)
+    phone_number = models.CharField(max_length=20, unique=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
+    age = models.PositiveIntegerField(null=True)
+
 
 class Todo(models.Model):
     title = models.CharField(max_length=200, unique=True)
-    description = models.TextField(blank=True)
+    description = models.TextField(null=True)
     is_completed = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
-    image = models.ImageField(upload_to='todo_images/')
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='images/', null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
--- apps/serializers.py
+++ apps/serializers.py
@@ -1,13 +1,26 @@
 from rest_framework import serializers
 from .models import User, Todo
 
+
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'phone_number', 'age', 'created_at']
+        fields = ('id', 'username', 'email', 'phone_number', 'created_at', 'age')
+
 
 class TodoSerializer(serializers.ModelSerializer):
-    owner = UserSerializer(read_only=True)
     class Meta:
         model = Todo
-        fields = ['id', 'title', 'description', 'is_completed', 'created_at', 'image', 'owner']
+        fields = ('id', 'title', 'description', 'is_completed', 'created_at', 'image', 'user')
+
+
+class TodoCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Todo
+        fields = ('title', 'description', 'image')
+
+
+class TodoUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Todo
+        fields = ('id', 'title', 'description', 'is_completed', 'image')
