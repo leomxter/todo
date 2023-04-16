8f7a59acfe662c7fd5e11b112705888a3b5e8406
Elmyrza Doolotbekov <elmyrza.do@gmail.com>
2023-04-16 19:06:27 +0600

    4th

--- apps/views.py
+++ apps/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import User, Todo
+from .serializers import UserSerializer, TodoSerializer
 
-# Create your views here.
+class UserList(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class TodoList(generics.ListCreateAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+class TodoDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
